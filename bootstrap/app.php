9ae02f4941d13fee249330d4d507de58bdd3c578
jdolezal <dolezal6@gmail.com>
2024-10-08 08:58:51 +0200

    Add package-lock.json to track dependencies
    Created a new package-lock.json file to ensure consistent dependency versions in the project. This will help with reproducible builds and better dependency management across different environments.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,9 @@
 <?php
 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: *');
+header('Access-Control-Allow-Headers: *');
+
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -11,7 +15,6 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
